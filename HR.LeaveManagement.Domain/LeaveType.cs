a56f726119f7dcc19c70e9aa06915ecca4682bf4
nthuy <thuynguyen.webdev@gmail.com>
2023-07-29 00:17:30 +0700

    create domain project

---
+++ HR.LeaveManagement.Domain/LeaveType.cs
@@ -0,0 +1,10 @@
+﻿using HR.LeaveManagement.Domain.Common;
+
+namespace HR.LeaveManagement.Domain;
+
+public class LeaveType : BaseEntity
+{
+    public required string Name { get; set; }
+
+    public int DefaultDays { get; set; }
+}
