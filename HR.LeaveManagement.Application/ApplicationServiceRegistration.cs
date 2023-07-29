e3fc0d11d559846d1d36041b42d1aff74a0fb203
nthuy <thuynguyen.webdev@gmail.com>
2023-07-29 13:28:10 +0700

    implement automapper

---
+++ HR.LeaveManagement.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace HR.LeaveManagement.Application;
+
+public class ApplicationServiceRegistration
+{
+    public static IServiceCollection AddApplicationServiceRegistration(IServiceCollection services)
+    {
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+        services.AddMediatR(config => config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+        return services;
+    }
+}
---
+++ HR.LeaveManagement.Application/Features/LeaveType/Queries/GetAllLeaveTypes/LeaveTypeDto.cs
@@ -0,0 +1,10 @@
+﻿namespace HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+
+public class LeaveTypeDto
+{
+    public int Id { get; set; }
+
+    public required string Name { get; set; }
+
+    public int DefaultDays { get; set; }
+}
