91c980a7655b12fc002156c7716f1b0cdba7433d
nthuy <thuynguyen.webdev@gmail.com>
2023-07-29 20:55:37 +0700

    add custom exception

--- HR.LeaveManagement.Application/Features/LeaveType/Commands/DeleteLeaveType/DeleteLeaveTypeCommandHandler.cs
+++ HR.LeaveManagement.Application/Features/LeaveType/Commands/DeleteLeaveType/DeleteLeaveTypeCommandHandler.cs
@@ -1,4 +1,5 @@
 ﻿using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.Exceptions;
 using MediatR;
 
 namespace HR.LeaveManagement.Application.Features.LeaveType.Commands.DeleteLeaveType;
@@ -18,6 +19,10 @@ public class DeleteLeaveTypeCommandHandler : IRequestHandler<DeleteLeaveTypeComm
         var leaveTypeToDelete = await _leaveTypeRepository.GetByIdAsync(request.Id);
 
         // verify that record is exists
+        if (leaveTypeToDelete == null)
+        {
+            throw new NotFoundException(nameof(leaveTypeToDelete), request.Id);
+        }
 
         // remove from database
         await _leaveTypeRepository.DeleteAsync(leaveTypeToDelete);
--- HR.LeaveManagement.Application/Features/LeaveType/Queries/GetLeaveTypeDetails/GetLeaveTypeDetailsQueryHandler.cs
+++ HR.LeaveManagement.Application/Features/LeaveType/Queries/GetLeaveTypeDetails/GetLeaveTypeDetailsQueryHandler.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.Exceptions;
 using MediatR;
 
 namespace HR.LeaveManagement.Application.Features.LeaveType.Queries.GetLeaveTypeDetails;
@@ -18,7 +19,14 @@ public class GetLeaveTypeDetailsQueryHandler : IRequestHandler<GetLeaveTypeDetai
 
     public async Task<LeaveTypeDetailsDto> Handle(GetLeaveTypeDetailsQuery request, CancellationToken cancellationToken)
     {
+        // query from db
         var leaveType = await _repository.GetByIdAsync(request.Id);
+
+        // verify that record exists
+        if (leaveType == null)
+        {
+            throw new NotFoundException(nameof(leaveType), request.Id);
+        }    
 
         var leaveTypeDetails = _mapper.Map<LeaveTypeDetailsDto>(leaveType);
 
